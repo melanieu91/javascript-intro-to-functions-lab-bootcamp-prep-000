da55d8d74f2a73d628fbec7d11f37605a340c21a
Melanie Uba <melanie.uba@gmail.com>
2017-12-03 20:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string) {
  var lowercase = 'hello!'
- lowercase.toLowerCase()=== lowercase
+ lowercase.toLowerCase() === lowercase
  var uppercase = "HELLO!"
  uppercase.toUpperCase() === uppercase
 if (sayHiToGrandma('hello!')){
