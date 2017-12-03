4acedb34d1f4bcf3ca8bd50f8bcb202c4e46f76a
Melanie Uba <melanie.uba@gmail.com>
2017-12-03 20:41:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+ console.log(`HELLO`)
+ console.log('spy')
+ return string.toUpperCase()
+}
+function logWhisper(string){
+  console.log(`hello`)
+  console.log('spy')
+  return string.toLowerCase()
+}
+function sayHitoGrandma(string) {
+ var lowercase = 'hello!'
+ lowercase.toLowerCase()=== lowercase
+ var uppercase = "HELLO!"
+ uppercase.toUpperCase() === uppercase
+
+}
